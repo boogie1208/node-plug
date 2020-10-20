fd5bb3f98491648ac9269338f17c4ee94926e1c4
denghf <1324519118@qq.com>
2020-10-20 23:35:53 +0800

    代码提交

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,42 +1,87 @@
 import message from './message.vue'
-let MessageConstructor = Vue.extend(message); // 写插件需要用到的
+// let MessageConstructor = Vue.extend(message); // 写插件需要用到的
+// let instance;
+// let instances = []; // 存放当前未close的message
+// const Message = function(options) {
+//   Vue.prototype.$message = message
+//   // 当前 Vue 实例是否运行于服务器
+//   if (Vue.prototype.$isServer) return;
+//   options = options || {};
+//   if (typeof options === 'string') {
+//     options = {
+//       message: options
+//     };
+//   }
+//    // 创建message实例,此时数据还没有挂载呢，$el 属性目前不可见，无法访问到数据和真实的dom
+//   instance = new MessageConstructor({
+//     data: options
+//   });
+//   instance.id = id;
+//   //判断instance.message是不是虚拟节点
+//   // if (isVNode(instance.message)) {
+//   //   instance.$slots.default = [instance.message];
+//   //   instance.message = null;
+//   // }
+//    //手动地挂载一个未挂载的实例。$mount(param)中param不存在时，模板将被渲染为文档之外的的元素，并且你必须使用原生 DOM API 把它插入文档中。
+//   instance.vm = instance.$mount();
+//   //用原生DOM API把它插入body中
+//   document.body.appendChild(instance.vm.$el);
+//   instance.vm.visible = true;
+//   instance.dom = instance.vm.$el;
+//   // css z-index层级叠加，覆盖之前已出现但还未close的message
+//   // instance.dom.style.zIndex = PopupManager.nextZIndex();
+//   instances.push(instance);
+//   return instance.vm;
+// };
 
-let instance;
-let instances = []; // 存放当前未close的message
-
-const Message = function(options) {
-  Vue.prototype.$message = message
-  // 当前 Vue 实例是否运行于服务器
-  if (Vue.prototype.$isServer) return;
-  options = options || {};
-  if (typeof options === 'string') {
-    options = {
-      message: options
-    };
-  }
-   // 创建message实例,此时数据还没有挂载呢，$el 属性目前不可见，无法访问到数据和真实的dom
-  instance = new MessageConstructor({
-    data: options
-  });
-  instance.id = id;
-
-  //判断instance.message是不是虚拟节点
-  // if (isVNode(instance.message)) {
-  //   instance.$slots.default = [instance.message];
-  //   instance.message = null;
-  // }
-   //手动地挂载一个未挂载的实例。$mount(param)中param不存在时，模板将被渲染为文档之外的的元素，并且你必须使用原生 DOM API 把它插入文档中。
-  instance.vm = instance.$mount();
-  //用原生DOM API把它插入body中
-  document.body.appendChild(instance.vm.$el);
-  instance.vm.visible = true;
-  instance.dom = instance.vm.$el;
-  // css z-index层级叠加，覆盖之前已出现但还未close的message
-  // instance.dom.style.zIndex = PopupManager.nextZIndex();
-  instances.push(instance);
-  return instance.vm;
+// // 给Message增加四个直接调用的方法
+// // 支持this.$message.success('xxx')方式调用，等同于this.$message({type: 'success',message: 'xxx'})
+// ['success', 'warning', 'info', 'error'].forEach(type => {
+//   Message[type] = options => {
+//     if (typeof options === 'string') {
+//       options = {
+//         message: options
+//       };
+//     }
+//     options.type = type;
+//     return Message(options);
+//   };
+// });
+const typeMap = {
+  success: 'success',
+  info: 'info',
+  warning: 'warning',
+  error: 'error'
 };
+const Message = {
+  install(Vue, props) {
+    let MessageConstructor = Vue.extend(message); // 写插件需要用到的
+    Vue.property.$message = message
+    Vue.component(message.name, message)
+    if (typeof props === 'string') {
+      props = {
+        message: props
+      };
+    }
+    function buildProps (args) {
+      let props = {}
+      props.message = args[0]
+      props.title = typeMap[props.type]
+      return props
+    }
 
+    function message () {
+      if (!arguments[0]) return
+      const propsData = buildProps(arguments)
+      const instance = new MessageConstructor({propsData})
+      document.body.appendChild(instance.$mount().$el)
+    }
+  }
+}
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Message = Message
+  Vue.use(Message)
+}
 // 给Message增加四个直接调用的方法
 // 支持this.$message.success('xxx')方式调用，等同于this.$message({type: 'success',message: 'xxx'})
 ['success', 'warning', 'info', 'error'].forEach(type => {
@@ -50,15 +95,4 @@ const Message = function(options) {
     return Message(options);
   };
 });
-
-// const Message = {
-//   install(Vue, options) {
-//     Vue.property.$message = message
-//     Vue.component(message.name, message)
-//   }
-// }
-// if (typeof window !== 'undefined' && window.Vue) {
-//   window.Message = Message
-//   Vue.use(Message)
-// }
 export default Message
